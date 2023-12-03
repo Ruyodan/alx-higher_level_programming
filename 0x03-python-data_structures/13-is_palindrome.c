e0c02307ca2fd8bf9433004bf5fdb75c51adf8f0
Ruyodan <salvator.hamdoune@gmail.com>
2023-12-03 03:11:57 -0800

    last check

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,53 +1,35 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
-*add_nodeint - adds a new node at the beginning of a listint_t list
-*@head: head of listint_t
-*@n: int to add in listint_t list
-*Return: address of the new element, or NULL if it failed
-*/
-listint_t *add_nodeint(listint_t **head, const int n)
+ * palindrome - utility for is_palindrome
+ * @top: pointer to a pointer to a singly linked list
+ * @next: pointer to a singly linked list
+ *
+ * Return: integer, 1 if palindrome, else 0
+ */
+int palindrome(listint_t **top, listint_t *next)
 {
-	listint_t *new;
+	int result = 0;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = *head;
-	*head = new;
-	return (new);
+	if (next == NULL)
+		return (1);
+
+	if (palindrome(top, next->next) && ((*top)->n == next->n))
+		result = 1;
+
+	*top = (*top)->next;
+
+	return (result);
 }
+
 /**
-*is_palindrome - identify if a syngle linked list is palindrome
-*@head: head of listint_t
-*Return: 1 if it is palindrome else 0
-*/
+ * is_palindrome - check if a singly linked list is a palindrome
+ * @head: linked list double pointer
+ *
+ * Return: integer, 1 if list is a palindrome else 0
+ */
 int is_palindrome(listint_t **head)
 {
-	listint_t *head2 = *head;
-	listint_t *aux = NULL, *aux2 = NULL;
-
-	if (*head == NULL || head2->next == NULL)
-		return (1);
-	while (head2 != NULL)
-	{
-		add_nodeint(&aux, head2->n);
-		head2 = head2->next;
-	}
-	aux2 = aux;
-	while (*head != NULL)
-	{
-		if ((*head)->n != aux2->n)
-		{
-			free_listint(aux);
-			return (0);
-		}
-		*head = (*head)->next;
-		aux2 = aux2->next;
-	}
-	free_listint(aux);
-	return (1);
+	return (palindrome(head, *head));
 }
+
