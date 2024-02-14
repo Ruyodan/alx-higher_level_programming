a62c92d728e1b060ad9cf58d1c862a9e2907bfd0
Ruyodan <salvator.hamdoune@gmail.com>
2024-02-14 03:31:03 -0800

    last check

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,9 @@
-sts all shows from hbtn_0d_tvshows_rate by their rating
--- lists all rows of a table by the sum of a linked row
-SELECT title, SUM(tv_show_ratings.rate) 'rating'
+-- List all shows from 'hbtn_0d_tvshows_rate' by their rating
+-- Each record should display tv_shows.title, rating sum
+-- Results must be sorted in ascending order by the rating
+-- You can use only one SELECT statement
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
-LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
-GROUP BY title
+INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_show_ratings.show_id
 ORDER BY rating DESC;
